00a271eff59871bf043b7de5b9fd61427dae202a
Eduardo <Eduardo@partnergrammer.com>
2019-12-04 16:54:06 -0700

    ajuste menu

--- resources/views/movil/habitaciones.blade.php
+++ resources/views/movil/habitaciones.blade.php
@@ -21,7 +21,7 @@ Hotels
     -webkit-box-shadow: 0px 0px 5px 0px rgba(166,166,166,1);
 -moz-box-shadow: 0px 0px 5px 0px rgba(166,166,166,1);
 box-shadow: 0px 0px 5px 0px rgba(166,166,166,1);
-position: absolute; 
+position: fixed; 
 bottom: 0; 
 width: 100vw; 
 padding: 0; 
